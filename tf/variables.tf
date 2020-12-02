2e9d7cca8efa605759ec636b85f0e218d39fd4bf
florianpialoux <git@mail.florianpialoux.ca>
2020-12-02 14:30:32 -0800

    - added more variables

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,9 +1,9 @@
 variable "project_id" {
+  default = "florian-test-297317"
   description = "The project ID to host the cluster in"
 }
 
 variable "region" {
+  default = "us-west1"
   description = "The region to host the cluster in"
 }
-project_id = "florian-test-297317"
-region     = "us-west1"
