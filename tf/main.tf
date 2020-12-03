4fe5225d688816da732149f60725fa35300173b3
florianpialoux <git@mail.florianpialoux.ca>
2020-12-02 16:08:49 -0800

    final version

---
+++ tf/main.tf
@@ -0,0 +1,41 @@
+variable "zone" {
+  description = "zone"
+}
+
+resource "google_container_cluster" "primary" {
+  name               = "my-cluster"
+  location           = var.zone
+  remove_default_node_pool = true
+  initial_node_count = 1
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+  resource "google_container_node_pool" "primary_preemptible_nodes" {
+    name       = "my-node-pool"
+    location   = var.zone
+    cluster    = google_container_cluster.primary.name
+    node_count = 3
+
+    node_config {
+      preemptible     = true
+      machine_type    = "g1-small"
+      image_type      = "COS"
+      disk_size_gb    = "30"
+      disk_type       = "pd-standard"
+      metadata        = {
+        disable-legacy-endpoints = "true"
+      }
+
+      oauth_scopes = [
+        "https://www.googleapis.com/auth/cloud-platform"
+      ]
+    }
+  }
---
+++ tf/terraform.tfvars
@@ -0,0 +1,3 @@
+project_id  = "florian-test-297317"
+region      = "us-west1"
+zone       = "us-west1-a"
