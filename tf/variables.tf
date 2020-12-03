4efb1d6f97327ff17643c4d2dbcf91b95a0d3e70
florianpialoux <git@mail.florianpialoux.ca>
2020-12-02 16:09:22 -0800

    removed unused files

--- tf/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id" {
-  default = "florian-test-297317"
-  description = "The project ID to host the cluster in"
-}
-
-variable "region" {
-  default = "us-west1"
-  description = "The region to host the cluster in"
-}
