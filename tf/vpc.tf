db17c8ad58a69fba84c57a9550ce612b3792eb08
florianpialoux <git@mail.florianpialoux.ca>
2020-12-03 11:11:03 -0800

    changed region to zone

--- tf/vpc.tf
+++ tf/vpc.tf
@@ -2,14 +2,14 @@ variable "project_id" {
   description = "project id"
 }
 
-variable "region" {
-  description = "region"
+variable "zone" {
+  description = "zone"
 }
 
 provider "google" {
   credentials = file("florian-test-297317-579f4bd03967.json")
   project = var.project_id
-  region  = var.region
+  region  = var.zone
 }
 
 # VPC
@@ -21,13 +21,13 @@ resource "google_compute_network" "vpc" {
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
   name          = "${var.project_id}-subnet"
-  region        = var.region
+  region        = var.zone
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
 
 }
 
-output "region" {
-  value       = var.region
-  description = "region"
+output "zone" {
+  value       = var.zone
+  description = "zone"
 }
