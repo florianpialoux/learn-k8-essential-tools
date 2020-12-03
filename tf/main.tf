1d84d2d8b5936bcf8eabf80aa681c993c7ba6bcf
florianpialoux <git@mail.florianpialoux.ca>
2020-12-03 11:25:55 -0800

    changed to region

--- tf/main.tf
+++ tf/main.tf
@@ -1,6 +1,6 @@
 resource "google_container_cluster" "primary" {
   name               = "my-cluster"
-  location           = var.zone
+  location           = var.region
   remove_default_node_pool = true
   initial_node_count = 1
 
@@ -16,7 +16,7 @@ resource "google_container_cluster" "primary" {
 
   resource "google_container_node_pool" "primary_preemptible_nodes" {
     name       = "my-node-pool"
-    location   = var.zone
+    location   = var.region
     cluster    = google_container_cluster.primary.name
     node_count = 3
 
