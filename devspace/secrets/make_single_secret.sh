6d2c2f236a2a6f441a7cb32f82b891ccb040effc
florianpialoux <git@mail.florianpialoux.ca>
2020-12-08 12:16:52 -0800

    renamed

--- devspace/secrets/make_single_secret.sh
+++ devspace/secrets/make_single_secret.sh
@@ -7,7 +7,6 @@ ENVIRONMENT='production'
 read -p "KEY: " KEY
 read -p "VALUE: " VALUE
 
-mkdir -p secrets
 KEY_T=$(echo "$KEY" | tr _ - | tr '[:upper:]' '[:lower:]')
 kubectl -n $NAMESPACE create secret generic $KEY_T \
   --dry-run=client \
