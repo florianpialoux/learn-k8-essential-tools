c88c29a91a062e5a3e042d83818e15ad9584773c
florianpialoux <git@mail.florianpialoux.ca>
2020-12-08 15:30:13 -0800

    fix var

--- devspace/secrets/make_multiple_secret.sh
+++ devspace/secrets/make_multiple_secret.sh
@@ -10,9 +10,9 @@ read -p "MySQL-Root-Password:" MYSQL_ROOT_PASSWORD
 
 kubectl -n $NAMESPACE create secret generic mysql-credentials \
   --dry-run=client \
-  --from-literal=mysql-password="$$MYSQL_PASSWORD" \
-  --from-literal=mysql-replication-password="$$MYSQL_REPLICATION_PASSWORD" \
-  --from-literal=mysql-root-password="$$MYSQL_ROOT_PASSWORD" \
+  --from-literal=mysql-password="$MYSQL_PASSWORD" \
+  --from-literal=mysql-replication-password="$MYSQL_REPLICATION_PASSWORD" \
+  --from-literal=mysql-root-password="$MYSQL_ROOT_PASSWORD" \
   -o json | \
   kubeseal \
     $SCOPE \
