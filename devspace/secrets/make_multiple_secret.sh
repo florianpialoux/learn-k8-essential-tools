11295f4b370d5e4c8ca569663f6a89919090f390
florianpialoux <git@mail.florianpialoux.ca>
2020-12-08 16:03:00 -0800

    added mariadb-password for wp

--- devspace/secrets/make_multiple_secret.sh
+++ devspace/secrets/make_multiple_secret.sh
@@ -7,12 +7,17 @@ ENVIRONMENT='production'
 read -p "MySQL-Password:" MYSQL_PASSWORD
 read -p "MySQL-Replication-Password:" MYSQL_REPLICATION_PASSWORD
 read -p "MySQL-Root-Password:" MYSQL_ROOT_PASSWORD
+# needed for bitnami/wordpress helmchart
+read -p "MySQL-Password-For-WP:" WORDPRESS_DATABASE_PASSWORD
+
 
 kubectl -n $NAMESPACE create secret generic mysql-credentials \
   --dry-run=client \
   --from-literal=mysql-password="$MYSQL_PASSWORD" \
   --from-literal=mysql-replication-password="$MYSQL_REPLICATION_PASSWORD" \
   --from-literal=mysql-root-password="$MYSQL_ROOT_PASSWORD" \
+# needed for bitnami/wordpress helmchart
+  --from-literal=mariadb-password="$WORDPRESS_DATABASE_PASSWORD" \
   -o json | \
   kubeseal \
     $SCOPE \
