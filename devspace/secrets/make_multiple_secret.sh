c5e692a6cf1e3e592be60b6424f8b4b2ceff4031
florianpialoux <git@mail.florianpialoux.ca>
2020-12-08 16:10:30 -0800

    fix comment

--- devspace/secrets/make_multiple_secret.sh
+++ devspace/secrets/make_multiple_secret.sh
@@ -7,9 +7,8 @@ ENVIRONMENT='production'
 read -p "MySQL-Password:" MYSQL_PASSWORD
 read -p "MySQL-Replication-Password:" MYSQL_REPLICATION_PASSWORD
 read -p "MySQL-Root-Password:" MYSQL_ROOT_PASSWORD
-# needed for bitnami/wordpress helmchart
-read -p "MySQL-Password-For-WP:" WORDPRESS_DATABASE_PASSWORD
-read -p "WP-Password:" WORDPRESS_PASSWORD
+read -p "MySQL-Password-For-WP:" WP_DATABASE_PASSWORD
+read -p "WP-Password:" WP_PASSWORD
 
 
 kubectl -n $NAMESPACE create secret generic mysql-credentials \
@@ -17,11 +16,10 @@ kubectl -n $NAMESPACE create secret generic mysql-credentials \
   --from-literal=mysql-password="$MYSQL_PASSWORD" \
   --from-literal=mysql-replication-password="$MYSQL_REPLICATION_PASSWORD" \
   --from-literal=mysql-root-password="$MYSQL_ROOT_PASSWORD" \
-# needed for bitnami/wordpress helmchart
-  --from-literal=mariadb-password="$WORDPRESS_DATABASE_PASSWORD" \
-  --from-literal=wordpress-password="$WORDPRESS_PASSWORD" \
+  --from-literal=mariadb-password="$WP_DATABASE_PASSWORD" \
+  --from-literal=wordpress-password="$WP_PASSWORD" \
   -o json | \
   kubeseal \
     $SCOPE \
     --controller-namespace kube-system \
-    > mysql-credentials.json
+    > credentials.json
